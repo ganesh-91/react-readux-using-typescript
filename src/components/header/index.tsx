0ee7709ce7722372ba8f46d908e0e96f8e680184
ganesh-91 <ganesh.khaire91@gmail.com>
2017-10-13 05:21:45 +0530

    added mobile screen menu bar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,42 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
-class Header extends React.Component< object> {
+class Header extends React.Component<{}, { showMenu: boolean }> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            showMenu: false
+        };
+    }
     render() {
         return (
-            <nav className="navbar navbar-expand-md fixed-top bg-dark">
+            <nav className="navbar navbar-expand-md navbar navbar-dark fixed-top bg-dark">
                 <a className="navbar-brand color-white">Dashboard</a>
-            </nav>
+                <div className="pos-f-t">
+                    <button className="navbar-toggler" type="button"
+                        onClick={() => {
+                            this.setState({ showMenu: !this.state.showMenu });
+                        }}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div className={"dropdown-menu header-menu " + (this.state.showMenu ? "show" : "")}>
+                    <ul className="nav nav-pills flex-column margin-left-10">
+                        <li className="nav-item">
+                            <Link to={`/user-list`}
+                                onClick={() => {
+                                    this.setState({ showMenu: false });
+                                }}>User list</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={`/report`}
+                                onClick={() => {
+                                    this.setState({ showMenu: false });
+                                }}>Report</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav >
         );
     }
 }
