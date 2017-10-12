c58bb6eed62ad0823c52cc484210802038c527c1
ganesh-91 <ganesh.khaire91@gmail.com>
2017-10-13 02:38:43 +0530

    navigation link added

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 class Sidebar extends React.Component<object> {
     render() {
         return (
             <div className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-                <h4>Total Applied</h4>
-                <p></p>
-                <h4>Total Hired</h4>
-                <p></p>
-                <h4>Total Not Hired</h4>
-                <p></p>
+                <ul className="nav nav-pills flex-column">
+                    <li className="nav-item">
+                        <Link to={`/user-list`}>User list</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={`/report`}>Report</Link>
+                    </li>
+                </ul>
             </div>
         );
     }
