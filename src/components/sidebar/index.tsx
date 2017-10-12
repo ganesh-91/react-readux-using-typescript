079b9024a000fe0206b19731a8afec5e5640a679
ganesh-91 <ganesh.khaire91@gmail.com>
2017-10-12 15:57:31 +0530

    added show report structure

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,34 +3,14 @@ import * as React from 'react';
 class Sidebar extends React.Component<object> {
     render() {
         return (
-            <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-                <ul className="nav nav-pills flex-column">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Reports</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Analytics</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Export</a>
-                    </li>
-                </ul>
-
-                <ul className="nav nav-pills flex-column">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Nav item</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Nav item again</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">One more nav</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Another nav item</a>
-                    </li>
-                </ul>
-            </nav>
+            <div className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+                <h4>Total Applied</h4>
+                <p></p>
+                <h4>Total Hired</h4>
+                <p></p>
+                <h4>Total Not Hired</h4>
+                <p></p>
+            </div>
         );
     }
 }
